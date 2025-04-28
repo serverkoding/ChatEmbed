eed91c71ea7f4722c3a4c3edf0b13435cd80d649
Muhammad Rizal Supriadi <mrsupriadi2000@gmail.com>
2025-04-28 10:35:37 +0700

    footer

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Server AI'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Aithor'}</span>
           </a>
         </span>
       </Show>
